da113f89d0cf81520649c42ba5f2ed2fa9cf4ba1
j-i-k-o <jiko.rogy@gmail.com>
2018-06-04 09:22:27 +0900

    make circuit.h

---
+++ include/circuit/circuit.h
@@ -0,0 +1,67 @@
+/**
+ * @file circuit.h
+ * @brief describing quantum circuits and quantum gates
+ * @author Kohji Nishimura
+ * @date 2018/06/03
+ *
+ */
+
+#pragma once
+#include <vector>
+#include <string>
+
+namespace qlib{
+
+	/**
+	 * quantum register
+	 */
+
+	struct qReg{
+	};
+
+	/**
+	 * Abstract components
+	 */
+
+	class Component{
+		private:
+			//! identifier name
+			std::string name;
+	};
+
+	/**
+	 * A class for representing quantum unitary gates.<br/>
+	 * Each unitary gate is identified by its name string.
+	 */
+
+	class Unitary : public Component{
+		private:
+			//! gate array
+			std::vector<qReg> q_registers;
+
+		public:
+			/**
+			 * return its inverse operator
+			 * @return inverse unitary operator
+			 */
+
+			virtual void dagger() const = 0;
+	};
+
+	/**
+	 * A class for representing measurement process.
+	 */
+
+	class Measurement : public Component{
+		private:
+			//! gate
+			qReg q_register;
+	};
+
+	/**
+	 * A class for representing quantum circuits
+	 */
+
+	class Circuit{
+	};
+}
