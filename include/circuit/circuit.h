5eaabeaf3973c6117943470d4ee625f35086fa66
j-i-k-o <jiko.rogy@gmail.com>
2018-06-05 20:29:33 +0900

    modify circuit.h and main.cpp

--- include/circuit/circuit.h
+++ include/circuit/circuit.h
@@ -9,66 +9,156 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <exception>
+#include <memory>
 
 namespace qlib{
+
+	//! abbrebiation of std::shared_ptr
+	template<class T>
+	using s_ptr = std::shared_ptr<T>;
+	//! abbrebiation of std::unique_ptr
+	template<class T>
+	using u_ptr = std::unique_ptr<T>;
+
+	//! default string of measurement
+	const static std::string default_str_measure = "__default_measure_";
+
+	/**
+	 * print exception message
+	 * @param exception_name exception name
+	 * @param message additional exception message
+	 * @return decorated string
+	 */
+	const std::string exception_format(const std::string& exception_name, const std::string& message){
+		std::string retmsg = exception_name + " Exception: \n" + message;
+		return retmsg;
+	}
+
+	/**
+	 * An exception class for QuantumCircuit library
+	 */
+
+	class InvalidOperationException: public std::exception{
+		private:
+			//! exception message
+			std::string message;
+
+		public:
+
+			/**
+			 * constructor of InvalidOperationException
+			 * @param msg additional exception message
+			 */
+
+			InvalidOperationException(const std::string& msg)
+				: message(exception_format("InvalidOperation", msg)){
+				}
+
+			virtual const char* what() const noexcept{
+				return message.c_str();
+			}
+	};
+
+
+	/**
+	 * abstract Component
+	 */
+
+	class Component{
+		protected:
+			//! an identifier
+			std::string name;
+			/**
+			 * protected constructor of Component <br/>
+			 * only friend class can call this constructor.
+			 * @param name identifier name
+			 */
+			Component(const std::string& name)
+				:name(name){
+				}
+	};
 
 	/**
 	 * quantum register
 	 */
 
-	struct qReg{
+	struct QuantumRegister{
 	};
 
 	/**
 	 * classical register
 	 */
 
-	struct cReg{
+	struct ClassicalRegister{
 	};
 
 	/**
-	 * Abstract components
+	 * class for Measurement operation
 	 */
 
-	class Component{
-		private:
-			//! identifier name
-			std::string name;
-	};
+	class Measure: public Component{
+		protected:
 
-	/**
-	 * A class for representing quantum unitary gates.<br/>
-	 * Each unitary gate is identified by its name string.
-	 */
+			//! pointer of QuantumRegister
+			const QuantumRegister* p_qreg;
+			//! pointer of ClassicalRegister
+			const ClassicalRegister* p_creg;
 
-	class Unitary : public Component{
-		private:
-			//! gate array
-			std::vector<qReg> q_registers;
-
-		public:
 			/**
-			 * return its inverse operator
-			 * @return inverse unitary operator
+			 * protected constructor of Measure
+			 * @param qreg QuntumRegister to be measured
+			 * @param creg corresponding ClassicalRegister
 			 */
 
-			virtual void dagger() const = 0;
+			Measure(const QuantumRegister& qreg, const ClassicalRegister& creg)
+			: Component(default_str_measure), p_qreg(&qreg), p_creg(&creg){
+			}
 	};
 
 	/**
-	 * A class for representing measurement process.
+	 * class for Unitary operator and Unitary container
 	 */
 
-	class Measurement : public Component{
-		private:
-			//! gate
-			qReg q_register;
+	class AbstUnitary : public Component{
+		protected:
+
 	};
 
 	/**
-	 * A class for representing quantum circuits
+	 * class for Unitary operator
+	 */
+
+	class UnitaryOp: public Component{
+		protected:
+			//! array of pointer of QuantumRegister
+			const std::vector<QuantumRegister*> p_qregs;
+
+			/**
+			 * protected constructor of UnitaryOp
+			 * @param name identifier name
+			 * @param 
+			 */
+
+			template<class... Args>
+			UnitaryOp(const std::string& name, const Args&... args)
+			: Component(name), p_qregs({(&args)...}){
+			}
+
+			template<class>
+
+	};
+
+	
+
+
+	/**
+	 * quantum circuit 
 	 */
 
 	class Circuit{
+		private:
+		public:
 	};
+
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "../include/circuit/circuit.h"
 
 int main(void) {
 	std::cout << "Hello world" << std::endl;
