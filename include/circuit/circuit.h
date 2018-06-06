b82991074910ff378d17ce7b541764bade7d9aee
j-i-k-o <jiko.rogy@gmail.com>
2018-06-06 14:31:25 +0900

    modify circuit.h

--- include/circuit/circuit.h
+++ include/circuit/circuit.h
@@ -8,18 +8,25 @@
 
 #pragma once
 #include <vector>
+#include <array>
 #include <string>
 #include <exception>
 #include <memory>
+#include <type_traits>
 
 namespace qlib{
 
-	//! abbrebiation of std::shared_ptr
+	/** abbrebiation of std::shared_ptr
+	 * @tparam T the type for shared_ptr
+	 */
 	template<class T>
-	using s_ptr = std::shared_ptr<T>;
-	//! abbrebiation of std::unique_ptr
+		using s_ptr = std::shared_ptr<T>;
+
+	/** abbrebiation of std::unique_ptr
+	 * @tparam T the type for unique_ptr
+	 */
 	template<class T>
-	using u_ptr = std::unique_ptr<T>;
+		using u_ptr = std::unique_ptr<T>;
 
 	//! default string of measurement
 	const static std::string default_str_measure = "__default_measure_";
@@ -84,6 +91,7 @@ namespace qlib{
 	 */
 
 	struct QuantumRegister{
+		//TODO: register num?
 	};
 
 	/**
@@ -91,6 +99,7 @@ namespace qlib{
 	 */
 
 	struct ClassicalRegister{
+		//TODO: register num?
 	};
 
 	/**
@@ -112,8 +121,8 @@ namespace qlib{
 			 */
 
 			Measure(const QuantumRegister& qreg, const ClassicalRegister& creg)
-			: Component(default_str_measure), p_qreg(&qreg), p_creg(&creg){
-			}
+				: Component(default_str_measure), p_qreg(&qreg), p_creg(&creg){
+				}
 	};
 
 	/**
@@ -122,34 +131,125 @@ namespace qlib{
 
 	class AbstUnitary : public Component{
 		protected:
-
+			/**
+			 * protected constructor of AbstUnitary
+			 * @param name identifier name
+			 */
+			AbstUnitary(const std::string& name)
+				: Component(name){
+				}
 	};
 
 	/**
-	 * class for Unitary operator
+	 * class for type_traits (all_same)
 	 */
 
-	class UnitaryOp: public Component{
-		protected:
-			//! array of pointer of QuantumRegister
-			const std::vector<QuantumRegister*> p_qregs;
+	template<typename... T>
+		struct all_same : std::false_type { };
 
-			/**
-			 * protected constructor of UnitaryOp
-			 * @param name identifier name
-			 * @param 
-			 */
+	/**
+	 * class for type_traits (all_same)
+	 */
 
-			template<class... Args>
-			UnitaryOp(const std::string& name, const Args&... args)
-			: Component(name), p_qregs({(&args)...}){
-			}
+	template<>
+		struct all_same<> : std::true_type { };
 
-			template<class>
+	/**
+	 * class for type_traits (all_same)
+	 */
 
-	};
+	template<typename T>
+		struct all_same<T> : std::true_type { };
 
-	
+	/**
+	 * class for type_traits (all_same)
+	 */
+
+	template<typename T, typename... Ts>
+		struct all_same<T, T, Ts...> : all_same<T, Ts...> { };
+
+	/**
+	 * class for Unitary operator
+	 * @tparam Ngate number of gate
+	 */
+
+	template<size_t Ngate>
+		class UnitaryOp: public AbstUnitary{
+			protected:
+				//! array of pointer of QuantumRegister
+				const std::array<QuantumRegister*, Ngate> p_qregs;
+				//! number of gates
+				const size_t num_gate = Ngate;
+
+				/**
+				 * protected constructor of UnitaryOp
+				 * @param name identifier name
+				 * @param args list of the points of QuantumRegister
+				 * @tparam Args list of the types of args. Each type must be QuantumRegister
+				 */
+
+				template<class... Args, typename = typename std::enable_if<all_same<Args...>::value, void>::type>
+					UnitaryOp(const std::string& name, const Args&... args)
+					: AbstUnitary(name), p_qregs({(&args)...}){
+					}
+		};
+
+	/**
+	 * class for Unitary operator with parameters
+	 * @tparam Ngate number of gate
+	 * @tparam Nparam number of parameters
+	 * @tparam T the type of parameter (assumed to be double)
+	 */
+
+	template<size_t Ngate, size_t Nparam, typename T>
+		class UnitaryOpParam: public UnitaryOp<Ngate>{
+			protected:
+				//! list of parameters
+				const std::array<T, Nparam> params;
+
+				/**
+				 * protected constructor of UnitaryOpParam
+				 * @param name identifier name
+				 * @param args list of the points of QuantumRegister
+				 * @tparam Args list of the types of args. Each type must be QuantumRegister
+				 */
+
+				template<class... Args, typename = typename std::enable_if<all_same<Args...>::value, void>::type>
+				UnitaryOpParam(const std::string& name, const std::array<T, Nparam>& params, const Args&... args)
+				: UnitaryOp<Ngate>(name, args...), params(params){
+				}
+		};
+
+
+		/**
+		 * class for Unitary container
+		 */
+		class UnitaryContainer: public AbstUnitary{
+			protected:
+				//! list of unitaries this class holds
+				std::vector<s_ptr<AbstUnitary>> unitaries;
+
+				/**
+				 * protected constructor of UnitaryContainer
+				 * @param name identifier name
+				 */
+
+				UnitaryContainer(const std::string& name)
+					:AbstUnitary(name){
+					}
+
+				/**
+				 * protected constructor of UnitaryContainer
+				 * @param name identifier name
+				 * @param unitaries list of unitary operators
+				 */
+
+				UnitaryContainer(const std::string& name, const std::vector<s_ptr<AbstUnitary>> unitaries)
+					:AbstUnitary(name), unitaries(unitaries){
+					}
+		};
+
+
 
 
 	/**
